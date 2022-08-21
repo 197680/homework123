990204c55de1556706e9d800470d91eebf253d15
197680 <bank_jobs@ukr.net>
2022-08-21 18:21:23 +0300

    Changed all files

--- index.js
+++ index.js
@@ -22,21 +22,56 @@ if (minutes < 10) {
 let h5 = document.querySelector("h5");
 h5.innerHTML = `${hours}:${minutes}, ${day}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-// Завдання2
+  return days[day];
+}
 
-//function search(event) {
-  //event.preventDefault();
-  //let searchInput = document.querySelector("#search-text-input");
-  //let h6 = document.querySelector("h6");
-  //if (searchInput.value) {
-    //h6.innerHTML = `${searchInput.value}`;
-  //} else {
-    //h6.innerHTML = `Zhytomyr`;
-  //}
-//}
-//let form = document.querySelector("#search-form");
-//form.addEventListener("submit", search);
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "cedbca303791d6afa0de69ad5e2a4b0b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function searchCity(city) {
     let apiKey = "cedbca303791d6afa0de69ad5e2a4b0b";
@@ -50,7 +85,6 @@ function handleSubmit(event) {
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
-
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -67,7 +101,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
+ getForecast(response.data.coord);
 }
 
 
